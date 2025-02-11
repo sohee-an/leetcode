b7191066b447cb608f82f1581f9e2681954b368f
so heee <58359509+sohee-an@users.noreply.github.com>
2025-02-11 16:06:27 +0900

    Time: 4401 ms (52.92%), Space: 18.1 MB (20%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,24 +1,33 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if not board or not board[0]:
-            return False
-        row=len(board)
-        col=len(board[0])
-        def backtrack(r,c,ind):
-            if len(word)==ind:
+        m = len(board)
+        n = len(board[0])
+        visited = [[False] * n for _ in range(m)]
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        
+        def dfs(x, y, idx):
+            if idx == len(word):
                 return True
-            if r<0 or r>=row or c<0 or c>=col or board[r][c]!=word[ind]:
+            
+            if x < 0 or x >= m or y < 0 or y >= n or visited[x][y] or board[x][y] != word[idx]:
                 return False
             
-            visited = board[r][c]
-            board[r][c]="#"
-
-            got = backtrack(r+1,c,ind+1) or backtrack(r-1,c,ind+1)or backtrack(r,c+1,ind+1) or backtrack(r,c-1,ind+1) 
-            board[r][c]=visited
-            return got
-
-        for i  in range(row):
-            for j in range(col):
-                if backtrack(i,j,0):
+            visited[x][y] = True
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if dfs(nx, ny, idx + 1):
                     return True
+            
+            visited[x][y] = False
+            return False
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    if dfs(i, j, 0):
+                        return True
+        
         return False
