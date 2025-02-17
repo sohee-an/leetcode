b56e19611b3664b003e054ed96685fac2f126979
so heee <58359509+sohee-an@users.noreply.github.com>
2025-02-17 11:20:58 +0900

    Time: 2 ms (53.82%), Space: 18.1 MB (77.05%) - LeetHub

---
+++ 1609-find-all-the-lonely-nodes/1609-find-all-the-lonely-nodes.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getLonelyNodes(self, root: Optional[TreeNode]) -> List[int]:
+        nodeValues=[]
+
+        def dfs(node):
+            if node is None:
+                return
+          
+
+            if node.left and node.right is None:
+                nodeValues.append(node.left.val)
+            elif node.right and node.left is None:
+                nodeValues.append(node.right.val)
+            
+            if node.left is None and node.right is None:
+                return 
+            
+
+            
+            dfs(node.left)
+            dfs(node.right)
+
+        
+        dfs(root)
+        
+        return nodeValues
+        
