d50be3d1a8fc505a89de4c0af2a49675d1755441
so heee <58359509+sohee-an@users.noreply.github.com>
2025-02-19 09:54:41 +0900

    Time: 333 ms (79.29%), Space: 89.1 MB (86.65%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -1,31 +1,61 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         
-        graph = [[] for _ in range(n)]
-        
-      
+        graph = {}
+    
         for a, b in edges:
+            if a not in graph:
+                graph[a] = []
             graph[a].append(b)
+            if b not in graph:
+                graph[b] = []
             graph[b].append(a)
-
-
+        
+       
         visited = [False] * n
         visited[source] = True
-
-        q=deque([source])
-
+        
+       
+        q = deque([source])
+        
         while q:
             current = q.popleft()
             if current == destination:
                 return True
                 
-            for next_node in graph[current]:
-                if not visited[next_node]:
-                    visited[next_node] = True
-                    q.append(next_node)
+         
+            if current in graph:  
+                for next_node in graph[current]:
+                    if not visited[next_node]:
+                        visited[next_node] = True
+                        q.append(next_node)
+        
+        return False
+        # graph = [[] for _ in range(n)]
+        
+      
+        # for a, b in edges:
+        #     graph[a].append(b)
+        #     graph[b].append(a)
+
+
+        # visited = [False] * n
+        # visited[source] = True
+
+        # q=deque([source])
+
+        # while q:
+        #     current = q.popleft()
+        #     if current == destination:
+        #         return True
+                
+        #     for next_node in graph[current]:
+        #         if not visited[next_node]:
+        #             visited[next_node] = True
+        #             q.append(next_node)
 
             
-        return False
+        # return False
                 
 
                 
