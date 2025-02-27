97c748282eea110d809e9d52a6a635de627bef42
so heee <58359509+sohee-an@users.noreply.github.com>
2025-02-27 09:45:25 +0900

    Time: 566 ms (10.56%), Space: 17.7 MB (85.95%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
@@ -4,10 +4,11 @@ class Solution:
         input_list=[]
         for i in range(1,n+1):
             input_list.append(i)
-
        
 
         for i in range(len(input_list)):
+            # sum_left = sum(range(1, i + 1)) 
+            #  sum_right = sum(range(i, n + 1)) 
             right=sum(input_list[:i+1])
             left=sum(input_list[i:])
          
