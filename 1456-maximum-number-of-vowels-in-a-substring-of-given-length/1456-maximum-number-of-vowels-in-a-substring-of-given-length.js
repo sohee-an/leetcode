777071cc93d2adf16d53cf17b3117130b5a2fe7a
so heee <58359509+sohee-an@users.noreply.github.com>
2026-02-02 09:33:48 +0900

    Time: 22 ms (26.21%), Space: 55.6 MB (90.97%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -4,26 +4,37 @@
  * @return {number}
  */
 var maxVowels = function(s, k) {
+    let s_set = new Set(['a','e','i','o','u']);
+    let max_sum = 0;
 
-    let s_set =new Set(['a','e','i','o','u'])
-
-    let max_sum=0
-    for (let i=0;i<k; i++){
-        if(s_set.has(s[i])){
-            max_sum++
+   
+    for (let i = 0; i < k; i++) {
+        if (s_set.has(s[i])) {
+            max_sum++;
         }
     }
-    let temp_sum=max_sum
+    
+   
+    if (max_sum === k) return k;
 
-    for (let i=k;i<s.length;i++){
-         if(s_set.has(s[i-k])){
-            temp_sum--
+    let temp_sum = max_sum;
+
+  
+    for (let i = k; i < s.length; i++) {
+        if (s_set.has(s[i - k])) {
+            temp_sum--;
         }
-        if(s_set.has(s[i])){
-            temp_sum++
+        if (s_set.has(s[i])) {
+            temp_sum++;
         }
-        max_sum=Math.max(temp_sum,max_sum)
+        
+        
+        if (temp_sum > max_sum) {
+            max_sum = temp_sum;
+            // 여기서 체크! max_sum이 k에 도달하면 더 볼 필요도 없습니다.
+            if (max_sum === k) return k;
+        }
     }
 
-    return max_sum
+    return max_sum;
 };
