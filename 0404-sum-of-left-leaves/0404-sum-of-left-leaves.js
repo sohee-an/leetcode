6ca7607e3e9ea86ddcfa65f38ddebe5964524765
so heee <58359509+sohee-an@users.noreply.github.com>
2026-02-27 16:20:06 +0900

    Time: 0 ms (100%), Space: 57.4 MB (39.31%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -13,25 +13,23 @@
 
 
 var sumOfLeftLeaves = function (root) {
+  
+   let queue=[root]
+   let result =0
+
+  
+   while (queue.length>0){
+      let node =queue.shift()
+
+ 
+      if(node.left && !node.left.left && !node.left.right){
+         result =result +node.left.val
+      }
 
 
-    if(!root) return 0
+      if(node.left)queue.push(node.left)
+      if(node.right)queue.push(node.right)
 
-    let queue=[root]
-    let result =0
-    
-    while (queue.length>0){
-        let node= queue.pop()
-
-        if(node.left ){
-            if(!node.left.left && !node.left.right){
-                  result+=node.left.val;
-            }
-           
-
-        }
-        if(node.left) queue.push(node.left)
-        if(node.right) queue.push(node.right)
-    }
-    return result
-};
+   }
+   return result
+}
