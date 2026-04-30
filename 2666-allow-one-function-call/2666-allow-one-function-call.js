d0de158027753cf76f7eb969b2e564a010fde211
so heee <58359509+sohee-an@users.noreply.github.com>
2026-04-30 22:28:52 +0900

    Time: 52 ms (13.7%), Space: 53.1 MB (82.97%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let flag=false
+    return function(...args){
+        console.log("art",args)
+       if(!flag){
+        flag=true
+        return fn(...args)
+       }
+       return undefined
+      
+        
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
