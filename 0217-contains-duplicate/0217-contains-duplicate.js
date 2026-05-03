1c52922d3b5e156cc1e06f33bfe09872f57aa564
so heee <58359509+sohee-an@users.noreply.github.com>
2026-05-03 17:55:44 +0900

    Time: 23 ms (29.39%), Space: 73 MB (86.64%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,20 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const hash = {};
-
-    for (let i = 0; i < nums.length; i++) {
-        let cur = nums[i];
-        
-     
-        if (hash[cur] !== undefined) {
-            return true; // 발견하는 즉시 종료
-        }
-        
-
-        hash[cur] = true;
-    }
-
+    const s=new Set([...nums])
   
-    return false;
+    console.log(nums.length)
+    if(s.size===nums.length){
+        return false
+    }else{
+        return true
+    }
 };
