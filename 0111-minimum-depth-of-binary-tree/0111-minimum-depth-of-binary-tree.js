25a945e51ec0681546f06b489c63eb720dea06da
so heee <58359509+sohee-an@users.noreply.github.com>
2026-05-11 10:55:46 +0900

    Time: 1 ms (93.77%), Space: 90 MB (98.73%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -10,20 +10,24 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var minDepth = function (root) {
-  if (!root) return 0;
+var minDepth = function(root) {
 
-  const queue = [[root, 1]]; 
-    while(queue.length>0){
+    if(!root)return 0
+
+    const queue=[[root,1]]
+    
+
+    while(queue.length >0){
         const [node,depth]=queue.shift()
 
-        //리프 인지 아닌지 확인
         if(!node.left && !node.right){
             return depth
         }
-        if(node.left) queue.push([node.left,depth+1])
-        if(node.right) queue.push([node.right,depth+1])
 
+        if(node.left)queue.push([node.left,depth+1])
+        if(node.right) queue.push([node.right,depth+1])
     }
-    return 0
+
+ 
+    
 };
