e9bf20c0f7f703b58966d024acbd9d5627e5d175
so heee <58359509+sohee-an@users.noreply.github.com>
2026-06-15 15:34:23 +0900

    Time: 4 ms (76.5%), Space: 55.5 MB (96.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,20 +3,19 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let start =0
+    let end = s.length-1
+ 
 
-    const result = s.toLowerCase().replace(/[^a-z0-9]/g, '');
-    
-    let start=0
-    let end =result.length -1
-
-    while (start<end){
-        if(result[start]!=result[end]){
-       
-            return false
-        }else{
+    while(start<end){
+        if(s[start]==s[end]){
             start++
             end--
+        }else{
+            return false
         }
     }
     return true
+    
 };
