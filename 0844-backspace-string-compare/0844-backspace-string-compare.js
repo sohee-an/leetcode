fdb76cd0d27f9d178880afbc466427ea858605ff
so heee <58359509+sohee-an@users.noreply.github.com>
2026-06-16 13:51:18 +0900

    Time: 2 ms (26.68%), Space: 54.9 MB (29.36%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -4,32 +4,28 @@
  * @return {boolean}
  */
 var backspaceCompare = function(s, t) {
-    let s_stack=[]
-    let t_stack=[]
 
-    for (char of s){
-        if(char=="#"){
-            s_stack.pop()
-        }else{
-               s_stack.push(char)
+    
+
+    function solution(str){
+        const stack=[]
+        for (const s of str){
+            if(s=="#"){
+                stack.pop()
+            }else{
+            stack.push(s)
+            }
+           
         }
-     
-    }
-        for (char of t){
-            console.log(char)
-        if(char=="#"){
-            t_stack.pop()
-        }else{
-        t_stack.push(char)
-        }
-       
+        
+        return stack.join('')
     }
 
-    const s_str = s_stack.join("");
-      const t_str = t_stack.join("");
+    if(solution(s)==solution(t)){
+        return true
+    }
+    return false
 
-
-      return s_str==t_str
 
     
 };
